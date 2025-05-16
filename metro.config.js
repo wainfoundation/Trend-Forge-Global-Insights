30c00a169cc6b0027aed29a99a5c65a13ec0e49f
Mrwain Organization <mrwainorganization@gmail.com>
2025-05-17 04:01:03 +0800

    Create metro.config.js

---
+++ metro.config.js
@@ -0,0 +1,9 @@
+const exclusionList = require('metro-config/src/defaults/exclusionList');
+
+module.exports = {
+  resolver: {
+    blacklistRE: exclusionList([
+      /AppData\/Local\/ElevatedDiagnostics\/.*/
+    ]),
+  },
+};
